38cbf9f6af5ee2df43a83aa12940f72887c02cb5
SamohinSV <serega.samox@gmail.com>
2020-05-07 00:16:02 +0400

    [server] add skeleton django rest-api.Сlosed #22

--- server/server/settings.py
+++ server/server/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Автоматизация настроек
 try:
