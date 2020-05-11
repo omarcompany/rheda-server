c3da075c0b5c2c4e9a5389f3ce1551bddccea2d7
Alexander Voykov <a.voykov@omprussia.ru>
2020-05-11 13:16:07 +0300

    Prepare code for PEP8
    Further setup of CI requires fixing all PEP8 errors in current code.
    
    Currently PEP8 linter emit some warning about imports:
    
      F403 'from .local_settings import *' used; unable to detect
      undefined names
      F401 '.local_settings.*' imported but unused
    
    Also some files have lines more than 80 characters.
    
    Relates #31

--- server/server/settings.py
+++ server/server/settings.py
@@ -86,16 +86,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'NumericPasswordValidator',
     },
 ]
 
@@ -118,9 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Автоматизация настроек
-try:
-    from .local_settings import *
-except ImportError:
-    pass
