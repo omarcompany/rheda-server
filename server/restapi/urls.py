af73bbb89f40f4bcf39199a8a802e27c224b9278
Danila <56697686+RostovtsevDanila@users.noreply.github.com>
2020-06-30 23:23:13 +0400

    Add sending message API Closed #44 (#47)

--- server/restapi/urls.py
+++ server/restapi/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, MessageView
 
 
 urlpatterns = [
     path("signup", UserView.as_view(), name="Create_Account"),
+    path("message", MessageView.as_view(), name="Send_Message"),
 ]
--- server/restapi/views.py
+++ server/restapi/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
-from .serializers import AccountSerializer
+from .serializers import AccountSerializer, MessageSerializer
 
 
 class UserView (generics.CreateAPIView):
     serializer_class = AccountSerializer
+
+
+class MessageView(generics.CreateAPIView):
+    serializer_class = MessageSerializer
