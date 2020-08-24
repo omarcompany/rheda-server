72bc2036d0ad617928004993695c985e568d7621
Danila Rostovtsev <rostovtsev.danila@gmail.com>
2020-08-24 19:37:06 +0400

    Add API get message list. Closed #48

--- server/restapi/tests.py
+++ server/restapi/tests.py
@@ -5,9 +5,9 @@ from django.test import Client
 class SignupTestCase(TestCase):
     def test_user_can_signup(self):
         """User can signup with any name and receive id"""
-        с = Client()
+        c = Client()
         request = {'name': 'Vlad МС'}
-        response = с.post('/api/signup', request)
+        response = c.post('/api/signup', request)
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json()["name"], request["name"])
@@ -44,3 +44,38 @@ class SendMessageTestCase(TestCase):
         self.assertEqual(senderResponse.status_code, 201)
         self.assertEqual(senderResponse.json()['text'],
                          senderRequest['text'])
+
+    def test_user_can_get_message_list(self):
+        """user can get message list"""
+        client = Client()
+
+        # Signup sender test user
+        sender_request = {'name': 'Dukalis'}
+        sender_response = client.post('/api/signup', sender_request)
+        sender_id = sender_response.json()['id']
+
+        # Signup recipient test user
+        recipient_request = {'name': 'Andryukha'}
+        recipient_response = client.post('/api/signup', recipient_request)
+        recipient_id = recipient_response.json()['id']
+
+        # Sending messages
+        messages = [
+            'Andryukha, we have a corpse.',
+            'Possibly a crime!',
+            'On the horses!'
+        ]
+        for message in messages:
+            sender_request = {
+                'sender': str(sender_id),
+                'recipient': str(recipient_id),
+                'text': message
+            }
+            client.post('/api/message', sender_request)
+
+        # Geting messages
+        get_response = client.get(
+            f'/api/messageList?recipient={recipient_id}').json()
+
+        for i in range(len(get_response)):
+            self.assertEqual(messages[i], get_response[i]['text'])
